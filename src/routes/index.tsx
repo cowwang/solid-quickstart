4e09bcfb8ad64b1d9962f314c8ac48651fc3c9eb
cowwang <129141234+cowwang@users.noreply.github.com>
2023-03-28 14:00:55 +0900

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <main>
       <Title>CowLotto</Title>
       <h1>Cowlotto!</h1>
-      <iframe width="800" height="1000" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vRTpCiSsjQW1yjLvHZzfJ7RnUD_f6RArjMwLEdANWynM-pdjvWJtEMzx-3a_TfC4Obpd9i67z3ly4RB/pubhtml?gid=696319208&amp;single=true&amp;widget=true&amp;headers=false"></iframe>
+      <iframe src="https://docs.google.com/spreadsheets/d/e/2PACX-1vRTpCiSsjQW1yjLvHZzfJ7RnUD_f6RArjMwLEdANWynM-pdjvWJtEMzx-3a_TfC4Obpd9i67z3ly4RB/pubhtml?gid=696319208&amp;single=true&amp;widget=true&amp;headers=false" width="800" height="1000"></iframe>
       <br>
       <Counter />
     </main>
