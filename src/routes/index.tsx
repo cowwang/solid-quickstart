532e1f8fca9442b4d76c8ce9345d6752040227fe
cowwang <129141234+cowwang@users.noreply.github.com>
2023-03-28 13:56:01 +0900

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,17 +4,10 @@ import Counter from "~/components/Counter";
 export default function Home() {
   return (
     <main>
+      <Title>CowLotto</Title>
+      <h1>Cowlotto!</h1>
       <iframe src="https://docs.google.com/spreadsheets/d/e/2PACX-1vRTpCiSsjQW1yjLvHZzfJ7RnUD_f6RArjMwLEdANWynM-pdjvWJtEMzx-3a_TfC4Obpd9i67z3ly4RB/pubhtml?gid=696319208&amp;single=true&amp;widget=true&amp;headers=false width=800 height=1000"></iframe>
-      <Title>Hello World</Title>
-      <h1>Hello world!</h1>
       <Counter />
-      <p>
-        Visit{" "}
-        <a href="https://start.solidjs.com" target="_blank">
-          start.solidjs.com
-        </a>{" "}
-        to learn how to build SolidStart apps.
-      </p>
     </main>
   );
 }
