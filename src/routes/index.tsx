74aa008596a99b6c94026c8c6f9e48b728d7d6dd
cowwang <129141234+cowwang@users.noreply.github.com>
2023-03-28 13:54:53 +0900

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,7 +4,7 @@ import Counter from "~/components/Counter";
 export default function Home() {
   return (
     <main>
-      <iframe src="https://docs.google.com/spreadsheets/d/e/2PACX-1vRTpCiSsjQW1yjLvHZzfJ7RnUD_f6RArjMwLEdANWynM-pdjvWJtEMzx-3a_TfC4Obpd9i67z3ly4RB/pubhtml?gid=696319208&amp;single=true&amp;widget=true&amp;headers=false"></iframe>
+      <iframe src="https://docs.google.com/spreadsheets/d/e/2PACX-1vRTpCiSsjQW1yjLvHZzfJ7RnUD_f6RArjMwLEdANWynM-pdjvWJtEMzx-3a_TfC4Obpd9i67z3ly4RB/pubhtml?gid=696319208&amp;single=true&amp;widget=true&amp;headers=false width=800 height=1000"></iframe>
       <Title>Hello World</Title>
       <h1>Hello world!</h1>
       <Counter />
